5893348329e4a6da4dbd43a94456657010592497
vinny <vinny.vip07@gmail.com>
2022-11-21 13:07:48 -0300

    feat: comment on interface

--- src/modules/auth/domain/interfaces/IAuthResponse.ts
+++ src/modules/auth/domain/interfaces/IAuthResponse.ts
@@ -1,5 +1,5 @@
 export default interface IAuthResponse {
   username: string;
-  password?: string; // opcional devido ao uso do delete no controller
+  password?: string; // opcional devido ao uso do delete no controller CreateAuth
   token: string;
 }
